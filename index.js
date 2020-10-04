03132fa473efca62c156b898376cac9af8fc863b
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-04 19:57:04 +0300

    More functionality

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-console.log("Hello There!")
+console.log("Hello There!");
 
 const getSum = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
+
+const getDiff = (a, b) => {
+  return a - b;
+};
+
+const getProduct = (a, b) => {
+  return a * b;
+};
